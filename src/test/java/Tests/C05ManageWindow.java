2265bd413f96f8b72dfc20f402d095a8409a5a7a
Lama-03 <Lama-4321-@hotmail.com>
2025-09-02 23:10:17 +0300

    Update repository from IntelliJ

--- src/test/java/Tests/C05ManageWindow.java
+++ src/test/java/Tests/C05ManageWindow.java
@@ -1,36 +1,44 @@
 package Tests;
-
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C05ManageWindow {
-    public static void main (String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
-        // Go to the w3school URL
+        //invoke chrome Browser
         WebDriver driver = new ChromeDriver();
 
-        driver.navigate().to("https://www.amazon.com");
-        Thread.sleep(1000);
+        // Go to the Amazon URL :
+        driver.navigate().to("https://www.amazon.com/");
 
-        // Print the position and size of the page
+
+        // Print the position and size of the page.
         Point dimensions = driver.manage().window().getPosition();
-        System.out.println("dimensions.getX() = " + dimensions.getX());
-        System.out.println("dimensions.getY() = " + dimensions.getY());
-        System.out.println("dimensions = " + dimensions);
+        System.out.println("Dimensions X :" + dimensions.getX());
+        System.out.println("Dimensions Y :" + dimensions.getY());
+        System.out.println("Dimensions = " + dimensions);
 
 
         Point position = driver.manage().window().getPosition();
-        System.out.println("position.getX() = " + position.getX());
-        System.out.println("position.getY() = " + position.getY());
+        System.out.println("position X : " + position.getX());
+        System.out.println("position Y : " + position.getY());
         System.out.println("position = " + position);
         Thread.sleep(1000);
 
 
-        // Adjust the position and size of the page as desired
-        driver.manage().window().setPosition(new org.openqa.selenium.Point(position.getX()- 50, position.getY()+ 50)); //- go to the left + go to the right
-        driver.manage().window().setSize(new org.openqa.selenium.Dimension(600, 600));
+        // Adjust the position and size of the page as desired.
+        driver.manage().window().setPosition(new org.openqa.selenium.Point(position.getX() + 100, position.getY()+ 100 )); //- go to the left + go to the right
+        driver.manage().window().setSize(new org.openqa.selenium.Dimension(500, 500));
         Thread.sleep(1000);
+
+
+
+        //Test that the page is in the position and size you want.
+        System.out.println("New Position : " + driver.manage().window().getPosition());
+        System.out.println("New Size : " + driver.manage().window().getSize());
+        Thread.sleep(3000);
 
         // Close the page
         driver.close();
