9a411aa30a5ba7cf926822f8aacca6c27c92ddc5
Lama-03 <Lama-4321-@hotmail.com>
2025-09-02 23:26:46 +0300

    Update repository from IntelliJ

---
+++ src/test/java/Tests/C06VerifyTitle.java
@@ -0,0 +1,33 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class C06VerifyTitle {
+    public static void main (String[] args) throws InterruptedException {
+
+        // invoke FirefoxDriver
+        WebDriver driver = new FirefoxDriver();
+
+        // Open URL of Website
+        //driver.navigate().to("https://www.youtube.com");
+        driver.navigate().to("https://youtu.be/QjuG1BIcq6k?si=jt-_zKQr6YYGAlwp");
+
+        //Get title
+        String Title = driver.getTitle();
+        System.out.println("Page Title is : " + Title );
+
+        // Verify title contains the word "video"
+        if (Title.toLowerCase().contains("video")) {
+            System.out.println("Test Passed: Title contains 'video'");
+        } else {
+            System.out.println("Test Failed: Title does not contain 'video'");
+        }
+
+
+        //close the Pag
+        driver.close(); // Close the active ATB
+        driver.quit(); //Terminates the session so closes all tabs
+}
+}
