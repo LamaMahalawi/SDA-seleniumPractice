566c93413c1c3943f459ed6bbe9a6e716d643bd5
Lama-03 <Lama-4321-@hotmail.com>
2025-09-16 15:23:17 +0300

    Update

---
+++ src/test/java/Tests/Junit_Selenium_MorningClass/Day06/C28_ScrollActions.java
@@ -0,0 +1,61 @@
+package Tests.Junit_Selenium_MorningClass.Day06;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import Tests.utilities.TestBase;
+
+public class C28_ScrollActions extends TestBase {
+    String url = "https://www.browserstack.com";
+    By testimonialsBy = By.id("testimonials-body");
+
+
+   @Test
+    public void scrollTest() throws InterruptedException {
+        driver.get(url);
+
+        Actions actions = new Actions(driver);
+       // JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        // Scroll page down with 'page down' key
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        Thread.sleep(2000);
+
+        // Scroll page down with 'down arrow' key
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        Thread.sleep(2000);
+
+        // Scroll page up with 'up arrow' key
+        actions.sendKeys(Keys.ARROW_UP).perform();
+        Thread.sleep(2000);
+
+        // Scroll page up with 'page up' key
+        actions.sendKeys(Keys.PAGE_UP).perform();
+        Thread.sleep(2000);
+
+        // Scroll page down with 'space' key
+        actions.sendKeys(Keys.SPACE).perform();
+        Thread.sleep(2000);
+
+        // Scroll to bottom of the page
+
+
+        // Scroll to view of an element
+        actions.scrollToElement(driver.findElement(testimonialsBy)).perform();
+        Thread.sleep(2000);
+
+        // Scroll to top of the page
+
+
+        // Scroll with moveToElement
+
+
+        // Scroll by some amount down
+        actions.scrollByAmount(0,200).perform();
+
+        // Scroll by some amount up
+
+    }
+}
+
---
+++ src/test/java/Tests/Junit_Selenium_MorningClass/Day08/C40_FileUpload02.java
@@ -0,0 +1,18 @@
+package Tests.Junit_Selenium_MorningClass.Day08;
+
+import Tests.utilities.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class C40_FileUpload02 extends TestBase {
+    /*
+    Go to URL: https://testpages.eviltester.com/styled/basic-html-form-test.html
+    Find the path of the file that you want to upload.
+    Click on Upload button.
+    Verify the upload message.
+*/
+
+    @Test
+    public void uploadTest() {
+
+    }
+}
